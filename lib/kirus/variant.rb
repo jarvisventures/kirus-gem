14113816b24426f4944ed1db4fbb86a4422c46f3
Jordan Widdison <jordan.widdison@gmail.com>
2018-01-27 14:05:39 -0700

    Remove Admin stuff, fix problem with order_item

--- lib/kirus/variant.rb
+++ lib/kirus/variant.rb
@@ -6,7 +6,7 @@ module Kirus
     self.headers['X-API-KEY'] = API_KEY
 
     belongs_to :product, class_name: "Kirus::Product"
-    has_many :order_items
+    has_many :order_items, class_name: "Kirus::OrderItem"
     # has_many :stocks
     # has_many :warehouses
     # has_many :option_value_variants
